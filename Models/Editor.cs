e470cc48a6f2666d9873845c128e9bbf68b36f48
Gameplushy <victor_f@orange.fr>
2023-11-20 14:59:34 +0100

    Creating DB entities

---
+++ Models/Editor.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GraphQL.Models
+{
+    public class Editor
+    {
+        [Key]
+        [GraphQLType(typeof(IdType))]
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public List<Game>? Games { get; set; }
+    }
+}
---
+++ Models/Game.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GraphQL.Models
+{
+    public class Game
+    {
+        [Key]
+        [GraphQLType(typeof(IdType))]
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required List<string> Genres { get; set; }
+        public int? PublicationDate { get; set; }
+        public required List<Editor> Editors { get; set; }
+        public required List<Studio> Studios { get; set; }
+        public required List<string> Platforms { get; set; }
+    }
+}
