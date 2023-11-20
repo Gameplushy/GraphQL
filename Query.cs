322646253169269401a6a8ac405618665d6bdc88
Gameplushy <victor_f@orange.fr>
2023-11-20 15:46:15 +0100

    Added query for Games. Also fixed joins

--- Query.cs
+++ Query.cs
@@ -1,14 +1,37 @@
 ﻿using GraphQL.Models;
 using GraphQL.Models.Responses;
+using Microsoft.EntityFrameworkCore;
 
 namespace GraphQL
 {
     public class Query
     {
-        const int PAGESIZE = 1;
+        const int PAGESIZE = 15;
 
-        public Studios? Studios([Service] DBContext dbContext, int page)
+        public Games? Games([Service] DBContext dBContext, int? page, string? genre, string? platform, string? studio)
         {
+            page = page ?? 1;
+            if (page < 1) return null;
+            IEnumerable<Game> targetedGames = dBContext.Games.Include(g=>g.Editors).Include(g=>g.Studios);
+            if (genre != null) targetedGames = targetedGames.Where(g => g.Genres.Contains(genre));
+            if (platform != null) targetedGames = targetedGames.Where(g => g.Platforms.Contains(platform));
+            if (studio != null) targetedGames = targetedGames.Where(g => g.Studios.Any(s=> s.Name == studio));
+            return new Games()
+            {
+                Infos = new Infos()
+                {
+                    Count = targetedGames.Count(),
+                    Pages = (int)Math.Ceiling((double)(targetedGames.Count() / PAGESIZE)),
+                    PreviousPage = page == 1 ? null : page - 1,
+                    NextPage = targetedGames.Count() <= page * PAGESIZE ? null : page + 1
+                },
+                Results = targetedGames.Skip(((int)page - 1) * PAGESIZE).Take(PAGESIZE).ToList()
+            };
+        }
+
+        public Studios? Studios([Service] DBContext dbContext, int? page)
+        {
+            page = page ?? 1;
             if (page < 1) return null;
             return new Studios()
             {
@@ -19,17 +42,18 @@ namespace GraphQL
                     PreviousPage = page == 1 ? null : page - 1,
                     NextPage = dbContext.Studios.Count() <= page * PAGESIZE ? null : page + 1
                 },
-                Results = dbContext.Studios.Skip((page - 1) * PAGESIZE).Take(PAGESIZE).ToList()
+                Results = dbContext.Studios.Include(s=>s.Games).Skip(((int)page - 1) * PAGESIZE).Take(PAGESIZE).ToList()
             };
         }
 
         public Studio? Studio([Service] DBContext dbContext, int id)
         {
-            return dbContext.Studios.FirstOrDefault(s => s.Id == id);
+            return dbContext.Studios.Include(s=>s.Games).FirstOrDefault(s => s.Id == id);
         }
 
-        public Editors? Editors([Service] DBContext dbContext, int page)
+        public Editors? Editors([Service] DBContext dbContext, int? page)
         {
+            page = page ?? 1;
             if(page < 1) return null;
             return new Editors()
             {
@@ -40,13 +64,13 @@ namespace GraphQL
                     PreviousPage = page == 1 ? null : page - 1,
                     NextPage = dbContext.Editors.Count() <= page*PAGESIZE ? null : page + 1
                 },
-                Results = dbContext.Editors.Skip((page - 1) * PAGESIZE).Take(PAGESIZE).ToList()
+                Results = dbContext.Editors.Include(e=>e.Games).Skip(((int)page - 1) * PAGESIZE).Take(PAGESIZE).ToList()
             };
         }
 
         public Editor? Editor([Service] DBContext dbContext, int id)
         {
-            return dbContext.Editors.FirstOrDefault(e => e.Id == id);
+            return dbContext.Editors.Include(e=>e.Games).FirstOrDefault(e => e.Id == id);
         }
     }
 }
