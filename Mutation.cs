2208b6fda8409a65868603369103b6ff2379c7e7
Gameplushy <victor_f@orange.fr>
2023-11-21 10:05:51 +0100

    CreateGame + Fix for id checks

--- Mutation.cs
+++ Mutation.cs
@@ -1,4 +1,5 @@
 ﻿using GraphQL.Models;
+using GraphQL.Models.Responses;
 
 namespace GraphQL
 {
@@ -9,7 +10,7 @@ namespace GraphQL
             if (gameIds != null)
             {
                 gameIds = gameIds.Distinct().ToList();
-                if (!dBContext.Games.All(g => gameIds.Contains(g.Id)))
+                if (!gameIds.All(gid=> dBContext.Games.Any(g => g.Id == gid)))
                     return null;
             }
 
@@ -23,7 +24,7 @@ namespace GraphQL
             if (gameIds != null)
             {
                 gameIds = gameIds.Distinct().ToList();
-                if (!dBContext.Games.All(g => gameIds.Contains(g.Id)))
+                if (!gameIds.All(gid => dBContext.Games.Any(g => g.Id == gid)))
                     return null;
             }
 
@@ -32,5 +33,30 @@ namespace GraphQL
             return newStudio.Entity;
         }
 
+        public Game? CreateGame([Service] DBContext dBContext, string name, List<string> genres, int? publicationDate, List<int> editors, List<int> studios, List<string> platforms)
+        {
+            genres = genres.Distinct().ToList();
+            editors = editors.Distinct().ToList();
+            studios = studios.Distinct().ToList();
+            platforms = platforms.Distinct().ToList();
+            if (!(studios.All(s=>dBContext.Studios.Any(stu=>stu.Id==s))) || !(editors.All(e=>dBContext.Editors.Any(edi => edi.Id == e))))
+                return null;
+            if (publicationDate != null)
+            {
+                if (!DateTime.TryParseExact(publicationDate.ToString(), "yyyyMMdd", null, System.Globalization.DateTimeStyles.None, out _))
+                    return null;
+            }
+            var newGame = dBContext.Games.Add(new Game()
+            {
+                Name = name,
+                PublicationDate = publicationDate,
+                Genres = genres,
+                Platforms = platforms,
+                Editors = editors.Select(id => dBContext.Editors.Single(e => e.Id == id)).ToList(),
+                Studios = studios.Select(id => dBContext.Studios.Single(s => s.Id == id)).ToList()
+            });
+            dBContext.SaveChanges();
+            return newGame.Entity;
+        }
     }
 }
