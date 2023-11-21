3bcfd1e51919466c0c15aa7941fb4a61785b6cde
Gameplushy <victor_f@orange.fr>
2023-11-21 10:47:17 +0100

    Patch Mutations for Studio and Editor

--- Mutation.cs
+++ Mutation.cs
@@ -1,5 +1,7 @@
 ﻿using GraphQL.Models;
 using GraphQL.Models.Responses;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.EntityFrameworkCore;
 
 namespace GraphQL
 {
@@ -19,6 +21,37 @@ namespace GraphQL
             return newEditor.Entity;
         }
 
+        public Editor? EditEditor([Service] DBContext dBContext, [ID] int id, string? name, List<int>? gameIds)
+        {
+            Editor? editor = dBContext.Editors.Include(e=>e.Games).SingleOrDefault(e => e.Id == id);
+            if (editor == null) return null;
+            if(name !=null)
+                editor.Name = name;
+            if(gameIds != null)
+            {
+                gameIds = gameIds.Distinct().ToList();
+                if (!gameIds.All(gid => dBContext.Games.Any(g => g.Id == gid)))
+                    return null;
+                //Weird thing that I have to do because I can't directly give a new List of int or there is an issue with primary keys...
+                List<Game> currentGameBase = editor.Games!;
+                int gameCounter = 0;
+                while(gameCounter < currentGameBase.Count) {
+                    if (!gameIds.Contains(currentGameBase[gameCounter].Id))
+                    {
+                        currentGameBase.Remove(currentGameBase[gameCounter]);
+                    }
+                    else
+                    {
+                        gameIds.Remove(currentGameBase[gameCounter].Id);
+                        gameCounter++;
+                    }
+                }
+                currentGameBase.AddRange(gameIds!.Select(id => dBContext.Games.Single(g => g.Id == id)));
+            }
+            dBContext.SaveChanges();
+            return editor;
+        }
+
         public Studio? CreateStudio([Service] DBContext dBContext, string name, List<int>? gameIds)
         {
             if (gameIds != null)
@@ -31,6 +64,38 @@ namespace GraphQL
             var newStudio = dBContext.Studios.Add(new Studio() { Name = name, Games = gameIds?.Select(id => dBContext.Games.Single(g => g.Id == id)).ToList() });
             dBContext.SaveChanges();
             return newStudio.Entity;
+        }
+
+        public Studio? EditStudio([Service] DBContext dBContext, [ID] int id, string? name, List<int>? gameIds)
+        {
+            Studio? studio = dBContext.Studios.Include(s=>s.Games).SingleOrDefault(s => s.Id == id);
+            if (studio == null) return null;
+            if (name != null)
+                studio.Name = name;
+            if (gameIds != null)
+            {
+                gameIds = gameIds.Distinct().ToList();
+                if (!gameIds.All(gid => dBContext.Games.Any(g => g.Id == gid)))
+                    return null;
+                //Weird thing that I have to do because I can't directly give a new List of int or there is an issue with primary keys...
+                List<Game> currentGameBase = studio.Games!;
+                int gameCounter = 0;
+                while (gameCounter < currentGameBase.Count)
+                {
+                    if (!gameIds.Contains(currentGameBase[gameCounter].Id))
+                    {
+                        currentGameBase.Remove(currentGameBase[gameCounter]);
+                    }
+                    else
+                    {
+                        gameIds.Remove(currentGameBase[gameCounter].Id);
+                        gameCounter++;
+                    }
+                }
+                currentGameBase.AddRange(gameIds!.Select(id => dBContext.Games.Single(g => g.Id == id)));
+            }
+            dBContext.SaveChanges();
+            return studio;
         }
 
         public Game? CreateGame([Service] DBContext dBContext, string name, List<string> genres, int? publicationDate, List<int> editors, List<int> studios, List<string> platforms)
